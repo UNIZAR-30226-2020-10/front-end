b0aebecfe5a93ee82b79ff50d33f03880d5d66a9
luisgg <luisgarcia98@hotmail.es>
2020-04-19 18:58:55 +0200

    cambio2

--- tuneit/lib/pages/showList.dart
+++ tuneit/lib/pages/showList.dart
@@ -1,8 +1,6 @@
-import 'package:expandable_bottom_bar/expandable_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:tuneit/classes/components/Playlist.dart';
 import 'package:tuneit/classes/components/Song.dart';
-import 'package:tuneit/classes/components/bottomExpandableAudio.dart';
 import 'package:tuneit/classes/values/Constants.dart';
 import 'package:tuneit/pages/audioPlayer.dart';
 import 'package:tuneit/widgets/OptionSongs.dart';
@@ -20,20 +18,23 @@ class ShowList extends StatefulWidget {
   ShowList({Key key, @required this.list_title,@required this.indetificadorLista}):super(key : key);
 }
 
-class _State extends State<ShowList> with SingleTickerProviderStateMixin {
+class _State extends State<ShowList> {
   SongLista songs= SongLista();
   String list_title;
   int indice;
   String indetificadorLista;
 
-  BottomBarController controller = null;
-  Duration _position;
-  Duration _duration;
   _State(this.indetificadorLista,this.list_title);
 
 
 
   void ObtenerDatos() async{
+
+          SongLista canciones =await fetchSonglists(indetificadorLista);
+          setState(() {
+            songs=canciones;
+          });
+
           await songs.fetchSonglists(indetificadorLista);
     }
 
@@ -46,7 +47,7 @@ class _State extends State<ShowList> with SingleTickerProviderStateMixin {
 
     super.initState();
     ObtenerDatos();
-    controller = BottomBarController(vsync: this, dragLength: 180, snap: true);
+
 
   }
 
@@ -82,76 +83,95 @@ class _State extends State<ShowList> with SingleTickerProviderStateMixin {
           children: <Widget>[
             Expanded(
               child:
-              StreamBuilder(
-                stream: songs.buscar_canciones_1,
-                builder: (context,snapshot){
-                  if(!snapshot.hasData){
-                    return Column(
-                      children: <Widget>[
-                      Image(image: AssetImage('assets/LogoApp.png'),
-                              fit: BoxFit.fill,
-                               width: 200,
-                                height: 200),
-                        Text("Buscando en nuestra base de datos las mejores canciones...")
+                   StreamBuilder<Object>(
+                     stream: songs.buscar_canciones_1,
+                     builder: (context, snapshot) {
+
+                       if(!snapshot.hasData){
+                         return Column(
+                           children: <Widget>[
+                             Image(image: AssetImage('assets/LogoApp.png'),
+                                 fit: BoxFit.fill,
+                                 width: 200,
+                                 height: 200),
+                             Text("Buscando en nuestra base de datos las mejores canciones...")
 
 
-                      ],
-                    );
-                  }
-                  else{
-                    return ListView.builder(
+                           ],
+                         );
+                       }
+                       else{
+                         return ReorderableListView(
 
-                    padding: const EdgeInsets.all(8),
-                    scrollDirection: Axis.vertical,
-                    itemCount: snapshot.data.songs.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        child: new ListTile(
-                         onTap:(){
+                           padding: const EdgeInsets.all(8),
+                           scrollDirection: Axis.vertical,
+                           onReorder: _onReorder,
 
+                           children: List.generate(
+                             songs.songs.length,
+                                 (index) {
+                               return
+                                 Card(
+                                   key: Key('$index'),
+                                   child: new ListTile(
+                                     onTap:(){
+                                       Navigator.of(context).push(MaterialPageRoute(
+                                         builder: (context) => PlayerPage(audios: songs.songs,indice: index,escanciones: true),
 
-                          Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => PlayerPage(audios: snapshot.data.songs,indice: index,escanciones: true),
+                                       ));
+                                     },
 
-                          ));
-                         },
+                                     leading: imagen_por_defecto(songs.songs[index].image),
+                                     title: Text(songs.songs[index].name),
+                                     subtitle: Text(juntarArtistas(songs.songs[index].artist)),
+                                     trailing: PopupMenuButton<String>(
+                                       onSelected: choiceAction,
+                                       itemBuilder: (BuildContext context){
+                                         return optionMenuSong.map((String choice){
+                                           return PopupMenuItem<String>(
+                                             value: (choice + "--"+songs.songs[index].id.toString()+"--"+indetificadorLista),
+                                             child: Text(choice),
+                                           );
 
-                          leading: imagen_por_defecto(snapshot.data.songs[index].image),
-                          title: Text(snapshot.data.songs[index].name),
-                          subtitle: Text(juntarArtistas(snapshot.data.songs[index].artist)),
-                          trailing: PopupMenuButton<String>(
-                            onSelected: choiceAction,
-                            itemBuilder: (BuildContext context){
-                              return optionMenuSong.map((String choice){
-                                return PopupMenuItem<String>(
-                                  value: (choice + "--"+snapshot.data.songs[index].id.toString()+"--"+indetificadorLista),
-                                  child: Text(choice),
-                                );
+                                         }).toList();
+                                       },
+                                     ),
 
-                              }).toList();
-                            },
-                          ),
+                                   ),
+                                 );
 
-                        ),
-                  );
-
-
-                  }
-                  );
-                  }
-    }
-              ),
-
-              ),
+                             },
+                           ),
 
 
-
-          ],
+                         );
+                       }
+                     }
+                   ),
     ),
-        bottomNavigationBar: bottomExpandableAudio()
+
+
+
+    ]
+
+
+              ),
+      bottomNavigationBar: NewWidget(),
+  );
+
+
+  }
+
+  void _onReorder(int oldIndex, int newIndex) {
+    setState(
+          () {
+        if (newIndex > oldIndex) {
+          newIndex -= 1;
+        }
+        final Song item = songs.songs.removeAt(oldIndex);
+        songs.songs.insert(newIndex, item);
+      },
     );
-
-
   }
 
   String juntarArtistas(List<String> datos){
@@ -222,4 +242,95 @@ class _State extends State<ShowList> with SingleTickerProviderStateMixin {
 
 
 
+}
+
+
+
+
+
+
+class NewWidget extends StatefulWidget {
+  const NewWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _NewWidgetState createState() => _NewWidgetState();
+}
+
+class _NewWidgetState extends State<NewWidget> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    Duration _position;
+    Duration _duration;
+    return new BottomAppBar(
+      child: Card(
+
+        child: Column(
+
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+
+          const ListTile(
+
+            leading: Icon(Icons.album, size: 50),
+
+            title: Text('Nombre de la Canción Correspondiente'),
+
+            subtitle: Text('Artista de la canción correspondiente'),
+
+            ),
+            SizedBox(
+              child: SliderTheme(
+                data: SliderThemeData(
+                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 5),
+                  trackHeight: 3,
+                  thumbColor: Colors.pink,
+                  inactiveTrackColor: Colors.grey,
+                  activeTrackColor: Colors.pink,
+                  overlayColor: Colors.transparent,
+                ),
+                child: Slider(
+                  value:
+                  _position != null ? _position.inMilliseconds.toDouble() : 0.0,
+                  min: 0.0,
+                  max:
+                  _duration != null ? _duration.inMilliseconds.toDouble() : 0.0,
+                  onChanged: (double value) async {
+                    //final Result result = await _audioPlayer
+                    //    .seekPosition(Duration(milliseconds: value.toInt()));
+                    //if (result == Result.FAIL) {
+                    //  print(
+                    //      "you tried to call audio conrolling methods on released audio player :(");
+                    //} else if (result == Result.ERROR) {
+                    //  print("something went wrong in seek :(");
+                    //}
+                    //_position = Duration(milliseconds: value.toInt());
+                  },
+                ),
+              ),
+            ),
+          new Row(mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(icon: Icon(Icons.skip_previous), onPressed: () {},),
+              IconButton(icon: Icon(Icons.repeat), onPressed: () {},),
+              IconButton(icon: Icon(Icons.play_circle_filled), onPressed: () {},),
+              IconButton(icon: Icon(Icons.shuffle), onPressed: () {},),
+              IconButton(icon: Icon(Icons.skip_next), onPressed: () {},)
+            ],
+
+        ),
+
+    ]),
+
+    )
+    );
+  }
 }
